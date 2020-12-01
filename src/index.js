c460c6c32603ead5b86f2f24e6be0779c8310fa4
zabrat <zaur1996@bk.ru>
2020-12-01 12:46:42 +0300

    finished html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import './style.less';
+ import './style.less';
+
+// class Reg{
+//     constructor(){
+//         this.userName = null;
+//         this.regFormEl = null;
+
+
+//         this.init();
+//     }
+
+//     init = () => {
+//         this.userName = input.value
+//     }
+
+//     createRegForm = () => {
+//         this.formEl = 
+//     }
+// }
+
+// new Reg();
