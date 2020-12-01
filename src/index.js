ee7f294a6f2ff03747c53dd87fbe200a88fd4b81
zabrat <zaur1996@bk.ru>
2020-12-01 15:29:41 +0300

    finished index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
  import './style.less';
 
-// class Reg{
-//     constructor(){
-//         this.userName = null;
-//         this.regFormEl = null;
+class Reg{
+    constructor(){
+        this.rootEl = null;
+        this.userInputEl = null;
+        this.passwordInputEl = null;
+        this.signUpButton = null;
 
+        this.init();
+    }
 
-//         this.init();
-//     }
+    init = () => {
+        this.rootEl = document.getElementById("root");
 
-//     init = () => {
-//         this.userName = input.value
-//     }
+        this.createRegForm();
 
-//     createRegForm = () => {
-//         this.formEl = 
-//     }
-// }
+        this.signUpButton = document.getElementById("signUp");
+        this.signUpButton.addEventListener("click", this.getInputData);
+        
 
-// new Reg();
+    }
+
+    createRegForm = () => {
+        const regContainer = document.createElement("div");
+        regContainer.classList = "reg-container";
+        const regEx = '(^(\+994[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$|[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$)';
+        regContainer.innerHTML = `
+        <form>
+                <h1>Registration form</h1>
+                <label for="email">Write Email</label>
+                <input type="text" name="email" id="email" title="Write just number phone or email">
+                <label for="password">Write password</label>
+                <input type="password" name="password" id="password">
+                <button type="submit" id="signUp">Sign up</button>
+        </form>`
+        this.rootEl.appendChild(regContainer);
+    }
+
+    getInputData = event => {
+        event.preventDefault();
+        this.userInputEl = document.getElementById("email").value;
+        this.passwordInputEl = document.getElementById("password").value;   
+        console.log(this.userInputEl, password)
+    }
+}
+
+new Reg();
