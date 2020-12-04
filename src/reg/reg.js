cdf161e9de98d0b740d37bf7f4c3a21ab50a4723
zabrat <zaur1996@bk.ru>
2020-12-04 20:06:39 +0300

    changed package.json

--- src/reg/reg.js
+++ src/reg/reg.js
@@ -15,13 +15,12 @@ class Reg{
 
     createRegForm = () => {
         this.regContainer = document.createElement("div");
-        this.regContainer.classList = "reg-container";
-        const regEx = '(^(\+994[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$|[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$)';
+        this.regContainer.classList = "reg-container";      
         this.regContainer.innerHTML = `
         <form>
             <h1>Registration</h1>
             <label for="user">Email or Number</label>
-            <input type="text" name="user" id="user" class="user" title="Write just number phone or email" pattern="${regEx}">
+            <input type="text" id="user" class="user" title="Write just number phone or email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
             <label for="password">Create password</label>
             <input type="password" name="password" id="password" class="password">
             <button type="submit" id="signUp" class="signUp">Sign up</button>
